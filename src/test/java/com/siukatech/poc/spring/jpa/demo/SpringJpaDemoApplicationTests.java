6a96d2dd982e7b36ab0e0271e0246344112994cf
siukatech <siuka114@gmail.com>
2025-02-16 02:47:33 +0800

    feat: initial commit

---
+++ src/test/java/com/siukatech/poc/spring/jpa/demo/SpringJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siukatech.poc.spring.jpa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
